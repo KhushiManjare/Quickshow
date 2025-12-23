72be4f441279227cdc9949ada4aad033398a2edd
KhushiManjare <khushimanjare65@gmail.com>
2025-12-23 19:21:54 +0530

    changed the vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,22 @@
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/",          // 🔥 THIS FIXES CSS ISSUE ON RENDER
+//   build: {
+//     outDir: "dist",
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/",          // 🔥 THIS FIXES CSS ISSUE ON RENDER
-  build: {
-    outDir: "dist",
-  },
+  plugins: [
+    react(),
+    tailwindcss(), // 🔥 REQUIRED FOR TAILWIND v4
+  ],
+  base: "/",
 });
